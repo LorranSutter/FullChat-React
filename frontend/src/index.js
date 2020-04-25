b3784674be864f2f80e336b66645c99d56687cfe
Lorran Sutter <sutterlorran@gmail.com>
2020-04-25 11:52:51 -0400

    Remove unecessary files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CookiesProvider } from 'react-cookie';
 
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
